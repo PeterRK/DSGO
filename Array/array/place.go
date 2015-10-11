b24e50c438462d384157a45d095ad13fd74a9453
Ruan Kunliang <PeterRK@126.com>
2015-10-11 21:09:14 +0800

    add something

---
+++ Array/array/place.go
@@ -0,0 +1,56 @@
+package array
+
+import (
+	"errors"
+	"math/rand"
+	"time"
+)
+
+func Pick(list []int, k int) (int, error) {
+	if k <= 0 || k > len(list) {
+		return 0, errors.New("out of range")
+	}
+	for begin, end := 0, len(list); begin < end-1; {
+		var pivot = list[(begin+end)/2] //一定要选偏后
+		var a, b = begin, end - 1
+		for { //注意对称性
+			for list[a] < pivot {
+				a++
+			}
+			for list[b] > pivot {
+				b--
+			}
+			if a >= b {
+				break
+			}
+			list[a], list[b] = list[b], list[a]
+			a++
+			b--
+		}
+		if k <= a {
+			end = a
+		} else {
+			begin = a
+		}
+	}
+	return list[k-1], nil
+}
+
+func Randomize(list []int) {
+	rand.Seed(time.Now().Unix())
+	for i := 1; i < len(list); i++ {
+		var j = rand.Int() % (i + 1)
+		list[i], list[j] = list[j], list[i]
+	}
+}
+
+func RandomPart(list []int, n int) {
+	var size = len(list)
+	if n > 0 && n < size {
+		rand.Seed(time.Now().Unix())
+		for i := n; i < size; i++ {
+			var j = rand.Int() % (i + 1)
+			list[i], list[j] = list[j], list[i]
+		}
+	}
+}
---
+++ Array/array/subsequence.go
@@ -0,0 +1,49 @@
+package array
+
+//最长递增子序列
+func LongestIncreasingSubsequence(src []int) []int {
+	var size = len(src)
+	if size == 0 {
+		return []int{}
+	} //result[i]记录某个长度为(i+1)的递增串的可能的最小尾数
+	var result = []int{src[0]}
+	for i := 1; i < size; i++ {
+		var place = SearchFirst(result, src[i])
+		if place == len(result) {
+			result = append(result, src[i])
+		} else {
+			result[place] = src[i]
+		}
+	}
+	return result
+}
+
+//最大子段和，全为负数时，最佳为空子段
+func MaximumIntervalSum(list []int) int {
+	var best, sum = 0, 0
+	for _, num := range list {
+		sum += num
+		if sum < 0 {
+			sum = 0
+		} else if sum > best {
+			best = sum
+		}
+	}
+	return best
+}
+
+func MaximumIntervalSumX(list []int) (value int, start int, end int) {
+	value, start, end = 0, 0, 0
+	var sum, mark = -1, -1
+	for i, num := range list {
+		if sum < 0 {
+			sum, mark = num, i
+		} else {
+			sum += num
+		}
+		if sum > value {
+			value, start, end = sum, mark, i+1
+		}
+	}
+	return value, start, end
+}
