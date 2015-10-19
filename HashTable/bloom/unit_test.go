309387c1f5f9ff1b05b87a3fe87fd38fd5c1230c
Ruan Kunliang <PeterRK@126.com>
2015-10-19 15:32:08 +0800

    update index

---
+++ HashTable/bloom/unit_test.go
@@ -0,0 +1,20 @@
+package bloom
+
+import (
+	"testing"
+)
+
+func assert(t *testing.T, state bool) {
+	if !state {
+		t.Fail()
+	}
+}
+func guard_ut(t *testing.T) {
+	if err := recover(); err != nil {
+		t.Fail()
+	}
+}
+
+func Test_Nothing(t *testing.T) {
+	//Do Nothing
+}
--- book/index.md
+++ book/index.md
@@ -15,7 +15,8 @@
 * 四、[Hash表](pages/04.md)
  - 4.1 [开链式Hash表](pages/04-A.md)
  - 4.2 [多路Hash表](pages/04-B.md)
- - 4.3 完全Hash
+ - 4.3 布隆过滤器
+ - 4.4 完全Hash
 * 五、[树](pages/05.md)
  - 5.1 [AVL树](pages/05-A.md)
  - 5.2 [红黑树](pages/05-B.md)
