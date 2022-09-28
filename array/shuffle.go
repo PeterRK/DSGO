3cfc654286e5319df2a9e5589ae781702239e0dd
Ruan Kunliang <PeterRK@126.com>
2022-09-28 22:41:46 +0800

    add RandomInts

--- array/shuffle.go
+++ array/shuffle.go
@@ -25,6 +25,33 @@ func RandomPartition[T any](list []T, n int) {
 	}
 }
 
+//随机组合，当m远小于m时比RandomPartition划算
+func RandomInts(n, m int) []int {
+	if m <= 0 || n <= m {
+		return nil
+	}
+	list := make([]int, m)
+	memo := make(map[int]int, m)
+	for m > 0 {
+		i := rand.Int() % n
+		a, got := memo[i]
+		if !got {
+			a = i
+		}
+		if i != n-1 {
+			b, got := memo[n-1]
+			if !got {
+				b = n-1
+			}
+			memo[i] = b
+		}
+		n--
+		m--
+		list[m] = a
+	}
+	return list
+}
+
 func Reverse[T any](list []T) {
 	for l, r := 0, len(list)-1; l < r; {
 		list[l], list[r] = list[r], list[l]
